e3ca2f71adca7875126bcc489ec8cdeac2655694
kenta.mori <morikenta.se@gmail.com>
2021-03-01 13:48:44 +0000

    0301

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
-// Route::group(['middleware' => ['auth']], function () {
-//     Route::resource('tasks', 'TasksController', ['only' => ['create'.'store', 'destroy']]);
-// });
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
+});
